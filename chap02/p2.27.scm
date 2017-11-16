fe2a152a019163cfa450fbb8efe84eb246d8566b
Arshad Khan <khan.m.arshad@gmail.com>
2017-11-16 17:01:37 +0800

    p2.27.scm

--- chap02/p2.27.scm
+++ chap02/p2.27.scm
@@ -10,5 +10,24 @@
 
 (define (deep-reverse l)
     (cond ((null? l) l)
-          ((pair? (car l)) (cons (reverse (car l)) (deep-reverse (cdr l))))
-          (else (reverse (cons (car l) (deep-reverse (cdr l)))))))
+          ((pair? (car l)) (cons (deep-reverse (car l)) (deep-reverse (cdr l))))
+          (else (cons reverse-1 l))))
+
+(define l1 (list (list 1 2 3) (list 4 5 6)))
+(define l2 (list 1 (list 2 3 4) 5 6 (list 7 8) 9))
+(define l3 (list (list (list (list 1 2 3))) (list 4 5)))
+
+(display l1)
+(display " -----> ")
+(display (deep-reverse l1))
+(newline)
+
+(display l2)
+(display " -----> ")
+(display (deep-reverse l2))
+(newline)
+
+(display l3)
+(display " -----> ")
+(display (deep-reverse l3))
+(newline)
